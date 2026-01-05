b58de020443730e69d29796e33845e56e6ea5921
AndrewLeach94 <andrew_leach@outlook.com>
2026-01-04 23:36:09 -0600

    refactor: update application and dues links in header and consts

--- src/consts.ts
+++ src/consts.ts
@@ -3,3 +3,8 @@
 
 export const SITE_TITLE = 'Krewe de Vélorévolte';
 export const SITE_DESCRIPTION = 'Ride! Revel! Rally!';
+
+export const BOHEME_DUES_URL = 'https://krewe-boheme.square.site/';
+export const VELOREVOLTE_PARTICIPANT_DUES_URL = 'https://www.paypal.com/ncp/payment/AZV62D7J47QL4';
+export const VELOREVOLTE_GENERAL_DUES_URL = 'https://www.paypal.com/ncp/payment/37LPFADJNCTN6';
+export const VELOREVOLTE_APPLICATION_URL = 'https://docs.google.com/forms/d/e/1FAIpQLSf5YbVz6M4kxTwqSqTzIThkG7RFaCDuVYNLHTk5804mjU7ZWw/viewform';
