632881aeaf6b8b93140eec18cfccbaa0e7ee4c71
Andrew Leach <51836067+AndrewLeach94@users.noreply.github.com>
2026-01-04 23:58:05 -0600

    Chore/Update Boheme Dues Link (#6)
    * update boheme link
    
    * refactor: update application and dues links in header and consts
    
    * update dues links with current URL

--- src/consts.ts
+++ src/consts.ts
@@ -3,3 +3,8 @@
 
 export const SITE_TITLE = 'Krewe de Vélorévolte';
 export const SITE_DESCRIPTION = 'Ride! Revel! Rally!';
+
+export const BOHEME_DUES_URL = 'https://krewe-boheme.square.site/';
+export const VELOREVOLTE_PARTICIPANT_DUES_URL = 'https://www.paypal.com/ncp/payment/DXE6C4BKP679W';
+export const VELOREVOLTE_GENERAL_DUES_URL = 'https://www.paypal.com/ncp/payment/VNM8YH8NRUS34';
+export const VELOREVOLTE_APPLICATION_URL = 'https://docs.google.com/forms/d/e/1FAIpQLSf5YbVz6M4kxTwqSqTzIThkG7RFaCDuVYNLHTk5804mjU7ZWw/viewform';
