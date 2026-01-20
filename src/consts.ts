cb09c1951ad6adaa7c879e1f23de61147a41805d
AndrewLeach94 <andrew_leach@outlook.com>
2026-01-19 21:26:47 -0600

    implement banner component

--- src/consts.ts
+++ src/consts.ts
@@ -8,3 +8,5 @@ export const BOHEME_DUES_URL = 'https://krewe-boheme.square.site/';
 export const VELOREVOLTE_PARTICIPANT_DUES_URL = 'https://www.paypal.com/ncp/payment/DXE6C4BKP679W';
 export const VELOREVOLTE_GENERAL_DUES_URL = 'https://www.paypal.com/ncp/payment/VNM8YH8NRUS34';
 export const VELOREVOLTE_APPLICATION_URL = 'https://docs.google.com/forms/d/e/1FAIpQLSf5YbVz6M4kxTwqSqTzIThkG7RFaCDuVYNLHTk5804mjU7ZWw/viewform';
+
+export const CURRENT_PARADE_DATE = '1/30/2025 19:00:00';
